60c9b52f48f92d4bb2c21018733035c75b73d5a1
esteban villa <estebana-villa@javeriana.edu.co>
2024-01-24 10:48:36 -0500

    Se agregar el menu inferior con los 5 iconos principales

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,119 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+/// Flutter code sample for [BottomAppBar].
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const BottomAppBarExampleApp());
 
-  // This widget is the root of your application.
+class BottomAppBarExampleApp extends StatelessWidget {
+  const BottomAppBarExampleApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomAppBarExample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BottomAppBarExample extends StatefulWidget {
+  const BottomAppBarExample({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<BottomAppBarExample> createState() =>
+      _BottomAppBarExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BottomAppBarExampleState
+    extends State<BottomAppBarExample> {
+  int _selectedIndex = 0; // cambia el valor inicial a 0
 
-  void _incrementCounter() {
+
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home', // cambia el texto a Home
+      style: optionStyle,
+    ),
+
+    Text(
+      'Index 1: Ruta',
+      style: optionStyle,
+    ),
+
+    Text(
+      'Index 2: Foro', // cambia el texto a Foro
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Perfil',
+      style: optionStyle,
+    ),
+
+    Text(
+      'Index 4: Mensajes',
+      style: optionStyle,
+    ),
+
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
+
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('JaveLab'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+
+      bottomNavigationBar: BottomAppBar( // usa el widget BottomAppBar
+        color: Color(0xFF2c5697), // agrega un color de fondo a la barra
+        child: Row( // usa un widget Row para colocar los ítems de la barra
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // distribuye los ítems de forma uniforme
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Spacer(), // agrega un espacio a la izquierda del primer ítem
+            IconButton( // usa un widget IconButton para crear un ítem con un ícono y una acción
+              icon: Icon(Icons.home), // cambia el ícono a home
+              color: _selectedIndex == 0 ? Colors.white : Color(0xFFDDDDDD), // cambia el color según el índice seleccionado
+              onPressed: () => _onItemTapped(0), // cambia el índice seleccionado al presionar el ítem
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Spacer(), // agrega un espacio entre el primer y el segundo ítem
+            IconButton( // usa otro widget IconButton para crear otro ítem
+              icon: Icon(Icons.school),
+              color: _selectedIndex == 1 ? Colors.white : Color(0xFFDDDDDD),
+              onPressed: () => _onItemTapped(1),
             ),
+            Spacer(), // agrega un espacio entre el segundo y el tercer ítem
+            IconButton( // usa otro widget IconButton para crear otro ítem
+              icon: Icon(Icons.forum_outlined), // cambia el ícono a forum_outlined
+              color: _selectedIndex == 2 ? Colors.white : Color(0xFFDDDDDD),
+              onPressed: () => _onItemTapped(2),
+            ),
+            Spacer(), // agrega un espacio entre el tercer y el cuarto ítem
+            IconButton( // usa otro widget IconButton para crear otro ítem
+              icon: Icon(Icons.account_circle_rounded),
+              color: _selectedIndex == 3 ? Colors.white : Color(0xFFDDDDDD),
+              onPressed: () => _onItemTapped(3),
+            ),
+            Spacer(), // agrega un espacio entre el cuarto y el quinto ítem
+            IconButton( // usa otro widget IconButton para crear otro ítem
+              icon: Icon(Icons.message_outlined),
+              color: _selectedIndex == 4 ? Colors.white : Color(0xFFDDDDDD),
+              onPressed: () => _onItemTapped(4),
+            ),
+            Spacer(), // agrega un espacio a la derecha del quinto ítem
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
