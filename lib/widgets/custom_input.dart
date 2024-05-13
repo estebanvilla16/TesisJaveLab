5568fcb82b05899704ae1137b1546a413bd3784b
santcamacho2002 <santicv130802@gmail.com>
2024-05-13 13:34:13 -0500

    Contraseña con puntos
    Ya no se puede ver la contraseña al momento de escribirla

--- lib/widgets/custom_input.dart
+++ lib/widgets/custom_input.dart
@@ -1,46 +1,50 @@
 import 'package:flutter/material.dart';
 
 class CustomInput extends StatelessWidget {
-   
-
-   final IconData icon;
-   final String placeholder;
-   final TextEditingController textController;
-   final TextInputType keyboardType;
-   final bool isPassword;
+  final IconData icon;
+  final String placeholder;
+  final TextEditingController textController;
+  final TextInputType keyboardType;
+  final bool isPassword;
 
   const CustomInput({
-    super.key, 
-    required this.icon, 
-    required this.placeholder, 
-    required this.textController, 
-    this.keyboardType = TextInputType.text, 
-    this.isPassword = false
-    });
+    Key? key,
+    required this.icon,
+    required this.placeholder,
+    required this.textController,
+    this.keyboardType = TextInputType.text,
+    this.isPassword = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-
       padding: const EdgeInsets.only(top: 5, left: 5, bottom: 5, right: 5),
       margin: const EdgeInsets.only(bottom: 20),
-      
-      child:  TextField(
+      child: TextField(
         controller: textController,
         autocorrect: false,
         keyboardType: keyboardType,
-
+        obscureText: isPassword, // Esta línea ocultará el texto si isPassword es true
+        // Condicional para mostrar el texto como puntos solo si isPassword es true
+        // Si isPassword es false, mostrará el texto normalmente
+        onChanged: (text) {
+          if (isPassword) {
+            textController.value = textController.value.copyWith(
+              text: text,
+              selection: TextSelection.collapsed(offset: text.length),
+            );
+          }
+        },
         decoration: InputDecoration(
-          prefixIcon:  Icon (icon),
+          prefixIcon: Icon(icon),
           hintText: placeholder,
-          contentPadding:  const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20.0), 
+            borderRadius: BorderRadius.circular(20.0),
           ),
         ),
-      )
-
-
+      ),
     );
   }
 }
